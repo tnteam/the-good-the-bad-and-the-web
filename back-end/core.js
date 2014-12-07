9a429e5e46ecd34d6a584e3abf651eab86ca5bdf
Rafik NACCACHE <rafik.naccache@gmail.com>
2014-12-07 19:58:28 +0100

    Added broadcasting api

--- back-end/core.js
+++ back-end/core.js
@@ -5,6 +5,7 @@ var game = require('game');
 var entities = require('entities')
 var players = require('players');
 var _ = require('underscore');
+var utils = require('utils');
 
 var app = require('express.io')();
 var board = new game.Board();
@@ -16,9 +17,9 @@ app.io.route('player', {
         var params = req.data;
         var player = new players.Player(params);
         var result = board.add_a_thing(board.players, player);
-        var response = {result: result, name: player.name};
-        console.log(response);
-        req.io.emit('player:register', response)
+        var response = {result: result, data: utils.transform_obj_to_name(player,'entities')};
+
+        app.io.broadcast('player:register', response)
     }
 });
 
@@ -31,12 +32,10 @@ app.io.route('entity', {
             return (pl.name == his_player_id);
         })
 
-        console.log('board.players: '+board.players[0].name);
-
-        var result=false;
+          var result = false;
         var entity_name = params.name
 
-        console.log('hisplayer '+his_player.name);
+
         if (his_player) {
             var new_entity = new entities.Entity(params);
             result = his_player.add_entity(new_entity);
@@ -47,14 +46,48 @@ app.io.route('entity', {
             }
         } // needs maybe better handling ?
 
-        response = {result : result, name : entity_name};
-        console.log(response);
-        req.io.emit('entity:add', response);
+        var ent_t_apts = utils.transform_obj_to_name(new_entity, 'aptitudes');
+        var response_data = utils.transform_obj_to_name(ent_t_apts, 'aptitudes');
+
+        response = {result: result, data: response_data};
+
+        app.io.broadcast('entity:add', response);
+    },
+
+    move: function (req) {
+        /*
+         ,{his_player_id :'rafikplayer',
+         name:'rafikentity',
+         x:10, y:15}
+         */
+        var params = req.data;
+        var entity_name = params.name;
+        var result = false;
+        var response;
+        var his_player_id = params.his_player_id;
+
+        var the_player = _.find(board.players, function (pl) {
+            return (pl.name == his_player_id)
+        });
+        var the_entity = _.find(the_player.entities, function (ent) {
+            return ent.name == entity_name
+        });
+        // the entity belongs to this player
+        if (the_entity) {
+            result = true;
+            the_entity.move(params.x, params.y);
+            board.remove_a_thing(the_player.entities, the_entity);
+            board.add_a_thing(the_player.entities, the_entity);
+        }
+        var ent_t_apts = utils.transform_obj_to_name(the_entity, 'aptitudes');
+        var response_data = utils.transform_obj_to_name(ent_t_apts, 'aptitudes');
+
+        response = {result: result, data: response_data};
+
+        app.io.broadcast('entity:move', response);
     }
 
-
 })
-
 
 
 app.get('/', function (req, res) {
