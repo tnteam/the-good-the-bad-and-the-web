41f2bca3cae2f6c253f69b7e739e316f7bf6d6b7
Rafik NACCACHE <rafik.naccache@gmail.com>
2014-12-08 04:32:29 +0100

    main game loop is ready for testing.

--- back-end/core.js
+++ back-end/core.js
@@ -25,7 +25,7 @@ app.io.route('player', {
         var name = params.name;
         var game_id = params.game_id;
         var webmana = parseInt(params.webmana);
-        var player_params = {name : name,game_id:game_id,entities:[],webmana :webmana};
+        var player_params = {name: name, game_id: game_id, entities: [], webmana: webmana};
         var player = new players.Player(player_params);
         var result = board.add_player(player);
         var response = {result: result, data: utils.transform_obj_to_name(player, 'entities')};
@@ -256,18 +256,18 @@ app.io.route('entity', {
                     source_entity: the_attack.source_entity.name, source_aptitude: the_attack.source_aptitude.name,
                     target_player: the_attack.target_player.name,
                     target_entity: the_attack.target_entity.name,
-                    target_vulnerability:the_attack.target_vulnerability.name
+                    target_vulnerability: the_attack.target_vulnerability.name
+                }
             }
         }
-    }
-    app.io.broadcast('entity:attack', {result: result, response: response});
+        app.io.broadcast('entity:attack', {result: result, response: response});
 
-},
+    },
 
-    transact: function(req) {
+    transact: function (req) {
         params = req.data;
         var result = false;
-            var response = {};
+        var response = {};
         source_player_id = params.source_player_id;
         source_entity_id = params.source_entity_id;
         source_aptitude_id = params.source_aptitude_id;
@@ -318,18 +318,19 @@ app.io.route('entity', {
         if (source_player && source_entity &&
             source_aptitude && target_player &&
             target_entity && target_aptitude &&
-        source_player_id != target_player_id) {
-            var the_transact = source_player.transact(source_entity,source_aptitude_id,
-                target_player,target_entity,target_aptitude);
+            source_player_id != target_player_id) {
+            var the_transact = source_player.transact(source_entity, source_aptitude_id,
+                target_player, target_entity, target_aptitude);
             if (the_transact && game.validate_transact(the_transact).valid) {
                 result = true
-                utils.add_a_thing(board.transacts,the_transact);
-                response  = {name:the_transact.name, source_player:source_player_id,
-                source_entity: source_entity_id,
-                source_aptitude : source_aptitude_id,
-                target_player:target_player_id,
-                    target_entity:target_entity_id,
-                    target_aptitude : target_aptitude_id
+                utils.add_a_thing(board.transacts, the_transact);
+                response = {
+                    name: the_transact.name, source_player: source_player_id,
+                    source_entity: source_entity_id,
+                    source_aptitude: source_aptitude_id,
+                    target_player: target_player_id,
+                    target_entity: target_entity_id,
+                    target_aptitude: target_aptitude_id
                 }
             }
 
@@ -343,35 +344,92 @@ app.io.route('entity', {
         attack_id = params.attack_id;
         defense_aptitude_id = params.defense_aptitude_id;
 
-        var result =false;
+        var result = false;
         var response = {};
 
-        the_attack = _.find(board.attacks,function(att){return (att.name == attack_id)});
-        the_defense_aptitude = _.find(rules.all_aptitudes(),function(def){return(def.name == defense_aptitude_id)});
+        the_attack = _.find(board.attacks, function (att) {
+            return (att.name == attack_id)
+        });
+        the_defense_aptitude = _.find(rules.all_aptitudes(), function (def) {
+            return (def.name == defense_aptitude_id)
+        });
 
         if (!the_attack || the_defense_aptitude) return;
-           target_player_id = the_attack.source_player.name;
-        target_player = _.find(board.players,function(pl){return (pl.name == target_player_id)});
-        if (target_player &&the_attack && the_defense_aptitude) {
-          var defense = target_player.defend(the_attack,the_defense_aptitude);
+        target_player_id = the_attack.source_player.name;
+        target_player = _.find(board.players, function (pl) {
+            return (pl.name == target_player_id)
+        });
+        if (target_player && the_attack && the_defense_aptitude) {
+            var defense = target_player.defend(the_attack, the_defense_aptitude);
             if (defense && game.validate_defense(defense).valid) {
-                result=true;
-                utils.add_a_thing(board.defenses,defense);
-                response = {name:defense.name,attack:the_attack.name,defense_aptitude:the_defense_aptitude};
+                result = true;
+                utils.add_a_thing(board.defenses, defense);
+                response = {name: defense.name, attack: the_attack.name, defense_aptitude: the_defense_aptitude};
             }
         }
-            app.io.broadcast('entity:defend', {result: result, response: response});
+        app.io.broadcast('entity:defend', {result: result, response: response});
     }
 
 });
 
-app.io.route('info',{
-    aptitudes : function(req){
+app.io.route('info', {
+    aptitudes: function (req) {
         var apts = rules.all_aptitudes();
-        req.io.emit('info:aptitudes',{aptitudes:apts})
+        req.io.emit('info:aptitudes', {aptitudes: apts})
     }
-})
+});
 
+app.io.route('game', {
+    start: function (req) {
+
+        setInterval(function () {
+            console.log('tick')
+            var defended_attacks = _.map(board.defenses, function (def) {
+                def.attack
+            });
+            var non_defended_attacks = _.difference(board.attacks, defended_attacks);
+
+            // defended attacks, I get outcome, apply it to source, dest, and remove defenses and attacks
+
+            _.map(board.defenses, function (def) {
+                var source = def.attack.source_entity;
+                var target = def.attack.target_entity;
+                var outcome = rules.possible_defenses[utils.hash_obj_tuple(def.attack.source_aptitude,
+                    def.defense_aptitude)];
+                source.webmana += outcome.source;
+                target.webmana += outcome.target;
+                app.io.broadcast('game:webmana', {
+                    source: source.name, source_webmana: source.webmana,
+                    target: target.name, target_webmana: target.webmana
+                });
+            })
+            board.defenses = [];
+
+            _.map(defended_attacks, function (att) {
+                board.attacks = _.without(board.attacks, att);
+                app.io.broadcast('game:attack-finished', att.name);
+            })
+
+// non defended attacks, I apply their outcome
+
+            _.map(non_defended_attacks, function (att) {
+                var source = att.source_entity;
+                var target = att.target_entity;
+
+                var outcome = rules.possible_attacks[utils.hash_obj_tuple(att.source_aptitude,
+                    att.target_vulnerability)];
+
+                source.webmana+=outcome.source;
+                target.webmana+=outcome.target;
+                app.io.broadcast('game:webmana', {
+                    source: source.name, source_webmana: source.webmana,
+                    target: target.name, target_webmana: target.webmana
+                });
+            })
+
+        }, 1000)
+    }
+});
 
 
 // TO DELETE
